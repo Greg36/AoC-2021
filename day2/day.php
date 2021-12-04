8d3de9554356bd4f6eaf6d7cd8feba17b98b1e56
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2021-12-04 14:43:14 +0100

    Day 2 done

--- day2/day.php
+++ day2/day.php
@@ -4,10 +4,49 @@ require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
 
+//$y = 0;
+//$z = 0;
+//
+//
+//foreach ( $input as $item ) {
+//	$dir = explode( ' ', $item );
+//	switch ( $dir[0] ) {
+//		case 'up':
+//			$z-= $dir[1];
+//			break;
+//		case 'down':
+//			$z+= $dir[1];
+//			break;
+//		case 'forward':
+//			$y+= $dir[1];
+//			break;
+//	}
+//}
 
 
+//echo 'Part 1: ' . $y * $z . PHP_EOL;
+
+$aim = 0;
+$y = 0;
+$z = 0;
+
+foreach ( $input as $item ) {
+	$dir = explode( ' ', $item );
+	switch ( $dir[0] ) {
+		case 'up':
+//			$z-= $dir[1];
+			$aim -= $dir[1];
+			break;
+		case 'down':
+//			$z+= $dir[1];
+			$aim += $dir[1];
+			break;
+		case 'forward':
+			$y+= $dir[1];
+			$z += ($aim * $dir[1]);
+			break;
+	}
+}
 
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
-
-// echo 'Part 2: ' . $correct . PHP_EOL;
+echo 'Part 2: ' . $y * $z . PHP_EOL;
