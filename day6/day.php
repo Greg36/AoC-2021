d35e69ce7949c0d6feeb3ec4b59fa88ea0ab0152
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2021-12-06 19:54:28 +0100

    Day 6 done

--- day6/day.php
+++ day6/day.php
@@ -4,10 +4,35 @@ require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
 
+$fishes = explode( ',', $input[0] );
+foreach ( $fishes as $key => $fish ) {
+	$fishes[$key] = (int) $fish;
+}
 
+//// Days
+//for ($i = 0; $i < 80; $i++) {
+//	foreach ( $fishes as $key => $fish ) {
+//		if( $fish === 0 ) {
+//			$fishes[$key] = 6;
+//			$fishes[] = 8;
+//		} else {
+//			$fishes[$key]--;
+//		}
+//	}
+//}
+//
+// echo 'Part 1: ' . count( $fishes ) . PHP_EOL;
 
+// Days
+for ( $i = 0; $i <= 8; $i++ ) $days[$i] = 0;
 
+// Initial fishes
+foreach( explode( ',', $input[0] ) as $fish )	$days[$fish]++;
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
+for($i = 0; $i < 256; $i++) {
+	$hatch = array_shift( $days );
+	$days[] = $hatch;
+	$days[6] += $hatch;
+}
 
-// echo 'Part 2: ' . $correct . PHP_EOL;
+echo 'Part 2: ' . array_sum( $days ) . PHP_EOL;
