80a041c84dba823c527f0bc7c0eed964b7e79df5
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2021-12-15 13:39:31 +0100

    Day 7 done

--- day7/day.php
+++ day7/day.php
@@ -3,11 +3,32 @@
 require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
+$input = explode( ',', $input[0] );
+
+//$fuel = [];
+//for ( $i = min( $input ); $i <= max($input); $i++ ) {
+//	foreach ( $input as $item ) {
+//		if( !isset( $fuel[$i] ) ) $fuel[$i] = 0;
+//		$fuel[$i] += abs(  $item - $i );
+//	}
+//}
+//sort( $fuel);
+//
+//echo 'Part 1: ' . $fuel[0] . PHP_EOL;
 
 
+function nth_triangle( $num ) {
+	return ( $num * $num + $num ) / 2;
+}
+$fuel = [];
+for ( $i = min( $input ); $i <= max($input); $i++ ) {
+	foreach ( $input as $item ) {
+		if( !isset( $fuel[$i] ) ) $fuel[$i] = 0;
+		$move = abs(  $item - $i );
+		$fuel[$i] += nth_triangle( $move );
+	}
+}
+sort( $fuel);
 
+echo 'Part 2: ' . $fuel[0] . PHP_EOL;
 
-
-// echo 'Part 1: ' . $correct . PHP_EOL;
-
-// echo 'Part 2: ' . $correct . PHP_EOL;
