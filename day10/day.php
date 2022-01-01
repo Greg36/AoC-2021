2e3f225ecb623dcf53f0b2e4a0122a61b8d79fbf
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-01-01 12:41:53 +0100

    Day 10 done

--- day10/day.php
+++ day10/day.php
@@ -3,11 +3,105 @@
 require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
+//$input = file( 'input2.txt', FILE_IGNORE_NEW_LINES );
+
+$scores = [
+	')' => 3,
+	']' => 57,
+	'}' => 1197,
+	'>' => 25137
+];
 
 
+function swap_symbol( $char ) {
+	switch ( $char ) {
+		case ')': return '(';
+		case ']': return '[';
+		case '}': return '{';
+		case '>': return '<';
+	}
+	return false;
+}
+
+function swap_symbol_back( $char ) {
+	switch ( $char ) {
+		case '(': return ')';
+		case '[': return ']';
+		case '{': return '}';
+		case '<': return '>';
+	}
+	return false;
+}
+
+//$score = 0;
+
+//foreach ( $input as $line ) {
+//	$stack = [];
+//	$line = str_split( $line);
+//	foreach ( $line as $char ) {
+//		if( preg_match( '/[(<[{]/', $char) ) {
+//			$stack[] = $char;
+//			continue;
+//		}
+//		if( preg_match( '/[)\]}>]/', $char) ) {
+//			if( array_slice($stack, -1)[0] == swap_symbol($char) ){
+//				array_pop( $stack );
+//			} else {
+//				$score += $scores[$char];
+//				break;
+//			}
+//		}
+//	}
+//}
+//
+//
+//
+// echo 'Part 1: ' . $score . PHP_EOL;
 
 
+$scores = [
+	')' => 1,
+	']' => 2,
+	'}' => 3,
+	'>' => 4
+];
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
+$totals = [];
 
-// echo 'Part 2: ' . $correct . PHP_EOL;
+foreach ( $input as $key => $line ) {
+	$stack = [];
+	$line = str_split( $line);
+	$corrupted = false;
+	foreach ( $line as $char ) {
+		if( preg_match( '/[(<[{]/', $char) ) {
+			$stack[] = $char;
+			continue;
+		}
+		if( preg_match( '/[)\]}>]/', $char) ) {
+			if( array_slice($stack, -1)[0] == swap_symbol($char) ){
+				array_pop( $stack );
+			} else {
+				$corrupted = true;
+				break;
+			}
+		}
+	}
+
+	// Incomplete line
+	if( ! empty( $stack ) && ! $corrupted ) {
+
+		$score = 0;
+		$stack = array_reverse( $stack );
+
+		foreach ( $stack as $item ) {
+			$score = $score * 5;
+			$score += $scores[ swap_symbol_back( $item ) ];
+		}
+		$totals[] = $score;
+	}
+}
+sort( $totals );
+$index = count( $totals );
+$index--;
+$index = floor($index / 2);
+ echo 'Part 2: ' . $totals[$index] . PHP_EOL;
