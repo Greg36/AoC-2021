e1056df8231f038d4601f9bf8eb2baf58a008557
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-01-03 21:30:44 +0100

    Day 17 done

--- day17/day.php
+++ day17/day.php
@@ -2,12 +2,108 @@
 
 require_once '../lib/lib.php';
 
-$input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
+$x = [50,76];
+$y = [-162, -134];
 
 
+// exaple
+//$x = [20, 30];
+//$y = [-10, -5];
 
 
+define( 'X', $x );
+define( 'Y', $y );
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
 
-// echo 'Part 2: ' . $correct . PHP_EOL;
+// negative y value cannot be greater the height of the area
+// when the current position is in y's range
+
+// X velocity cannot be greater than distance to right side of target's area
+$x_max = max( $x );
+$x_min = get_min_x( min( $x ) );
+
+
+function get_min_x( $x ) {
+	$num = 0;
+	while( $x >= 0  ) {
+		$x -= $num;
+		$num++;
+	}
+	return $num - 1;
+}
+
+function in_target( $point ) {
+	global $in_target;
+	if( $point['x'] >= min(X) && $point['x'] <= max(X) && $point['y'] >= min(Y) && $point['y'] <= max(Y) ) {
+		$in_target[] = [$point['x'], $point['y']];
+		return true;
+	}
+	return false;
+}
+
+function missed_target( $point ){
+	if( $point['x'] > max(X) || $point['y'] < min(Y) ) return true;
+	return false;
+}
+
+global $in_target;
+$in_target = [];
+
+$max_y = 0;
+$initial_cords = [];
+
+// For each available x
+for ($i = $x_min; $i <= 64; $i++) {
+
+	// Fire with y velocity between 1 and 50
+	for ($k = min( Y ); $k <= abs( min( Y ) ) - 1; $k++) {
+
+		$vx = $i;
+		$vy = $k;
+
+		$h = 0;
+
+		// Fire the shot
+		$point = [ 'x' => 0, 'y' => 0 ];
+		while ( true ) {
+			if ( missed_target( $point ) ) break;
+
+			// Take step
+			$point['x'] += $vx;
+			$point['y'] += $vy;
+
+			if( in_target( $point ) ) {
+				if( $h > $max_y ) {
+					$max_y = $h;
+					$initial_cords = [
+						'x' => $i,
+						'y' => $k
+					];
+				}
+				break;
+			}
+
+			// Save height
+			if( $point['y'] > $h ) $h = $point['y'];
+
+			// Decrease speeds
+			$vy--;
+			if( $vx !== 0 ) {
+				if( $vx > 0 ) {
+					$vx--;
+				} else {
+					$vx++;
+				}
+			}
+
+		}
+	}
+}
+
+
+// 1406 is to high
+// 783 is to low
+
+echo 'Part 1: ' . $max_y . PHP_EOL;
+
+ echo 'Part 2: ' . count( $in_target ) . PHP_EOL;
