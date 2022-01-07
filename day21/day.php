d6eb55ebf8515960b9500ee47b68eb6899d06fcd
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-01-07 19:14:38 +0100

    Day 21 part 1

--- day21/day.php
+++ day21/day.php
@@ -4,10 +4,43 @@ require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
 
+$player1 = 1;
+$player2 = 3;
 
+//$player1 = 4;
+//$player2 = 8;
 
+$score1 = 0;
+$score2 = 0;
+$rolls = 0;
 
+$dice = 1;
+while ( $score1 < 1000 && $score2 < 1000 ) {
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
+	for ($i = 1; $i<=3; $i++) {
+		$player1 += $dice;
+		$dice++;
+		$rolls++;
+		if( $dice > 100 ) $dice -= 100;
+		if( $player1 > 10 ) $player1 = $player1 % 10;
+		if( $player1 === 0 ) $player1 = 10;
+	}
+	$score1 += $player1;
+	if( $score1 >= 1000 ) break;
+
+	for ($i = 1; $i<=3; $i++) {
+		$player2 += $dice;
+		$dice++;
+		$rolls++;
+		if( $dice > 100 ) $dice -= 100;
+		if( $player2 > 10 ) $player2 = $player2 % 10;
+		if( $player2 === 0 ) $player2 = 10;
+	}
+	$score2 += $player2;
+}
+
+$looser = min( $score1, $score2 );
+
+ echo 'Part 1: ' . ( $looser * $rolls ) . PHP_EOL;
 
 // echo 'Part 2: ' . $correct . PHP_EOL;
