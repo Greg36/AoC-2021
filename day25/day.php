10d768ab67e3022d3ca8149d71373d4546c5f6b6
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-01-09 23:02:07 +0100

    Day 25 part 1 done

--- day25/day.php
+++ day25/day.php
@@ -3,11 +3,77 @@
 require_once '../lib/lib.php';
 
 $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
+//$input = file( 'input2.txt', FILE_IGNORE_NEW_LINES );
+//$input = file( 'input3.txt', FILE_IGNORE_NEW_LINES );
 
 
+$map = [];
+foreach ( $input as $row ) {
+	$map[] = str_split( $row );
+}
+
+$height = count( $map );
+$width = count( $map[0] );
+
+$i = 0;
+while( true ) {
+	$move = 0;
+
+	$temp = $map;
+	foreach ( $temp as $y => $row ) {
+		foreach ( $row as $x => $cell ) {
+
+			if( $cell == '>' ) {
+				// Check map right edge
+				if( $x == $width - 1 ) {
+					if( $row[0] == '.' ) {
+						$map[$y][0] = '>';
+						$map[$y][$x] = '.';
+						$move++;
+					}
+				} else {
+					if( $row[$x + 1] == '.' ) {
+						$map[$y][$x + 1] = '>';
+						$map[$y][$x] = '.';
+						$move++;
+					}
+				}
+			}
+		}
+	}
 
 
+	$a = 'xd';
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
+	$temp = $map;
+
+	foreach ( $temp as $y => $row ) {
+		foreach ( $row as $x => $cell ) {
+			if( $cell == 'v' ) {
+
+				// Check map bottom edge
+				if( $y == $height - 1 ) {
+					if( $temp[0][$x] == '.' ) {
+						$map[0][$x] = 'v';
+						$map[$y][$x] = '.';
+						$move++;
+					}
+				} else {
+					if( $temp[$y + 1][$x] == '.' ) {
+						$map[$y + 1][$x] = 'v';
+						$map[$y][$x] = '.';
+						$move++;
+					}
+				}
+			}
+		}
+	}
+
+	$i++;
+
+	if( $move === 0 ) break;
+}
+
+ echo 'Part 1: ' . $i . PHP_EOL;
 
 // echo 'Part 2: ' . $correct . PHP_EOL;
