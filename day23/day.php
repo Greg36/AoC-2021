da2069ec8911a06ba5a626a2fbe0cc7c84a8b137
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-01-09 13:38:45 +0100

    Day 23 done by hand

--- day23/day.php
+++ day23/day.php
@@ -7,7 +7,12 @@ $input = file( 'input.txt', FILE_IGNORE_NEW_LINES );
 
 
 
+/*
+ * Apart from going brute force with A*
+ * I had no idea how to code this but I
+ * managed to solve it by hand in
+ * around 2h
+ */
+ echo 'Part 1: ' . 15160 . PHP_EOL;
 
-// echo 'Part 1: ' . $correct . PHP_EOL;
-
-// echo 'Part 2: ' . $correct . PHP_EOL;
+ echo 'Part 2: ' . 46772 . PHP_EOL;
